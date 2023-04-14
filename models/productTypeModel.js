89f5ff64bb8d1e9740003da0cf6b1b376658b2b7
bhautik <bhautikjani1102@gmail.com>
2023-04-14 14:25:13 +0530

    final project updated

--- models/productTypeModel.js
+++ models/productTypeModel.js
@@ -4,7 +4,7 @@ const productTypeSchema = new mongoose.Schema({
   productType: {
     type: String,
     unique: [true, 'productType allready exist..'],
-    enum: ['shampoo', 'capsule', 'oximeter', 'device'],
+    enum: ['devices', 'medicine', 'shampoo', 'masks'],
   },
 });
 
